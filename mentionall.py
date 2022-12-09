9405a2996ac53beeedbbdd330b338284251b1580
Arifbrat <89793610+Arifbrat@users.noreply.github.com>
2022-12-09 09:48:29 +0400

    Update mentionall.py

--- mentionall.py
+++ mentionall.py
@@ -538,16 +538,22 @@ async def mentionall(event):
 
 
 @client.on(events.NewMessage(pattern="^/admins ?(.*)"))
-async def tag_admin(event):
-    chat = await event.get_input_chat()
-    text = "♕︎Adminlər Siyahısı♕︎"
-    async for x in event.client.iter_participants(chat, 5, filter=ChannelParticipantsAdmins):
-        text += f"[{x.first_name}](tg://user?id={x.id})"
-    if event.reply_to_msg_id:
-        await event.client.send_message(event.chat_id, text, reply_to=event.reply_to_msg_id)
-    else:
-        await event.reply(text)
-    raise StopPropagation
+async def mentionall(tagadmin):
+
+	if tagadmin.pattern_match.group(1):
+		seasons = tagadmin.pattern_match.group(1)
+	else:
+		seasons = ""
+
+	chat = await tagadmin.get_input_chat()
+	a_=0
+	await tagadmin.delete()
+	async for i in client.iter_participants(chat, filter=ChannelParticipantsAdmins):
+		if a_ == 500:
+			break
+		a_+=5
+		await tagadmin.client.send_message(tagadmin.chat_id, "**[{}](tg://user?id={}) {}**".format(i.first_name, i.id, seasons))
+		sleep(0.5)
 
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------------#
 
